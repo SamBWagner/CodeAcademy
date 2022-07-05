3d502c1dc17f365fb0b1b76f970b2a4e689b91c6
Sam Wagner <66365977+Kyleskiller@users.noreply.github.com>
2022-07-06 07:41:21 +1000

    Added whaleTalk.js

---
+++ whaleTalk.js
@@ -0,0 +1,19 @@
+const input = "turpentine turtles";
+const vowels = ["a", "e", "i", "o", "u"];
+const resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(vowels[j]);
+    }
+  }
+  if (input[i] === 'e' || input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+}
+
+const resultString = resultArray.join("").toUpperCase();
+
+console.log(resultString);
+
