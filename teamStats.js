067158b630ac4f7cded3c66a1665eb85a5ef20cf
Sam Wagner <66365977+Kyleskiller@users.noreply.github.com>
2022-07-09 10:18:11 +1000

    Update teamStats.js (Formatting)

--- teamStats.js
+++ teamStats.js
@@ -2,61 +2,44 @@
  * JavaScript course.
  */
 const team = {
-    _players: [
-      {
-        firstName: "",
-        lastName: "",
-        age: 0,
-      },
-      {
-        firstName: "",
-        lastName: "",
-        age: 0,
-      },
-      {
-        firstName: "",
-        lastName: "",
-        age: 0,
-      },
-    ],
-    _games: [
-      {
-        opponent: '',
-        teamPoints: 0,
-        opponentPoints: 0
-      },
-      {
-        opponent: '',
-        teamPoints: 0,
-        opponentPoints: 0
-      },
-      {
-        opponent: '',
-        teamPoints: 0,
-        opponentPoints: 0
-      }
-    ],
-  
-  
-    get players() {
-      return _players;
-    },
-  
-    get games() {
-      return _games;
-    },
-  
-    //simple adder, no input validation
-    addPlayer (newFirstName, newLastName, newAge) {
-      this._players.push({firstName: newFirstName,lastName: newLastName, age: newAge});
-    },
-  
-    //simple adder, no input validation
-    addGame(newOpponent, newTeamPoints, newOpponentPoints) {
-      this._games.push({opponent:newOpponent, teamPoints:newTeamPoints, opponentPoints:newOpponentPoints});
-    }
-  };
-  
+  //_Variables
+  _players: [ { firstName: "", lastName: "", age: 0, },
+              { firstName: "", lastName: "", age: 0, },
+              { firstName: "", lastName: "", age: 0, },],
+  _games: [ { opponent: '', teamPoints: 0, opponentPoints: 0 },
+            { opponent: '', teamPoints: 0, opponentPoints: 0 },
+            { opponent: '', teamPoints: 0, opponentPoints: 0 }],
+
+
+  // Methods
+  get players() {
+    return _players;
+  },
+
+  get games() {
+    return _games;
+  },
+
+  //simple adder, no input validation
+  addPlayer (newFirstName, newLastName, newAge) {
+    this._players.push({firstName: newFirstName,lastName: newLastName, age: newAge});
+  },
+
+  //simple adder, no input validation
+  addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+    this._games.push({
+      opponent:newOpponent, 
+      teamPoints:newTeamPoints, 
+      opponentPoints:newOpponentPoints}
+      );
+  }
+};
+
+
+
+
+
+  //Main
   team.addPlayer('Christiano', 'Ronaldo', 31);
   team.addGame('Titans', 100, 98);
   console.log(team._players);
